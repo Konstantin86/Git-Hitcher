12d9ebdf8368e30f634a45dfb62ddfb0460d856b
Konstantin86 <kostyan22@gmail.com>
2015-04-05 01:12:32 +0300

    Adjusted search alg

---
+++ Source/Hitcher.DataAccess/Extensions/RouteExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Hitcher.DataAccess.Entities;
+
+namespace Hitcher.DataAccess.Extensions
+{
+  public static class RouteExtensions
+  {
+    public static double[] ParseStartCoords(this Route route)
+    {
+      return route.StartLatLng.Split(',').Select(double.Parse).ToArray();
+    }
+
+    public static double[] ParseEndCoords(this Route route)
+    {
+      return route.EndLatLng.Split(',').Select(double.Parse).ToArray();
+    }
+  }
+}
--- Source/Hitcher/Service/RouteService.cs
+++ Source/Hitcher/Service/RouteService.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.Linq;
 using Hitcher.DataAccess;
 using Hitcher.DataAccess.Entities;
+using Hitcher.DataAccess.Extensions;
 using Hitcher.Models.Request;
 using Hitcher.Utils;
 
@@ -51,11 +52,37 @@ namespace Hitcher.Service
     {
       var routes = _unitOfWork.RouteRepository.GetAll(m => m.Type == 0).Include(m => m.Coords).ToList();
 
-      var distances = routes.ToDictionary(route => route.Id, route => route.Coords.Select(m => DistanceAlgorithm.DistanceBetweenPlaces(startLng, startLat, m.Lng, m.Lat)).Min() + route.Coords.Select(m => DistanceAlgorithm.DistanceBetweenPlaces(endLng, endLat, m.Lng, m.Lat)).Min());
+      var distances = routes.ToDictionary(route => route.Id, route => RouteDistanceSelector(startLat, startLng, endLat, endLng, route));
 
-      var top2Dist = distances.OrderBy(m => m.Value).Select(m => m.Key).Take(resultsCount);
+      var top2Dist = distances.Where(m => m.Value > 0).OrderBy(m => m.Value).Select(m => m.Key).Take(resultsCount);
 
       return routes.Where(r => top2Dist.Contains(r.Id));
+    }
+
+    private static double RouteDistanceSelector(float startLat, float startLng, float endLat, float endLng, Route route)
+    {
+      double minFromStart = route.Coords.Select(m => DistanceAlgorithm.DistanceBetweenPlaces(startLng, startLat, m.Lng, m.Lat)).Min();
+      double minFromEnd = route.Coords.Select(m => DistanceAlgorithm.DistanceBetweenPlaces(endLng, endLat, m.Lng, m.Lat)).Min();
+
+      double[] routeStartLatLng = route.ParseStartCoords();
+      double routeStartLat = routeStartLatLng[0];
+      double routeStartLng = routeStartLatLng[1];
+
+      double[] routeEndLatLng = route.ParseEndCoords();
+      double routeEndLat = routeEndLatLng[0];
+      double routeEndLng = routeEndLatLng[1];
+
+      double startFromStart = DistanceAlgorithm.DistanceBetweenPlaces(startLng, startLat, routeStartLng, routeStartLat);
+      double endFromEnd = DistanceAlgorithm.DistanceBetweenPlaces(endLng, endLat, routeEndLng, routeEndLat);
+
+      // Filter distances:
+      if (minFromStart > 5 || minFromEnd > 5)
+      {
+        return 0;
+      }
+
+      //return minFromStart + minFromEnd;
+      return minFromStart + minFromEnd + startFromStart + endFromEnd;
     }
   }
 }
