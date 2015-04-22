2dc6215619dea954e6a9cf4727fb6e33fca8a6e2
Konstantin86 <kostyan22@gmail.com>
2015-04-22 14:40:59 +0300

    Bugfixing, switching to dev mode

--- Source/Hitcher/Controllers/AuthController.cs
+++ Source/Hitcher/Controllers/AuthController.cs
@@ -241,7 +241,6 @@ namespace Hitcher.Controllers
         string userName = Translit(externalLogin.UserName).Replace(" ", string.Empty);
         // TODO temporary workaround. It's better to redirect user to associate view to allow him provide e-mail
         
-        
         user = new AppUser { UserName = userName, Email = externalLogin.Email, EmailConfirmed = true, JoinDate = DateTime.Now };
 
         user.Email = string.IsNullOrEmpty(user.Email) ? "test@t.ua" : user.Email;
@@ -528,8 +527,8 @@ namespace Hitcher.Controllers
 
     public static string Translit(string str)
     {
-      string[] lat_up = { "A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "Kh", "Ts", "Ch", "Sh", "Shch", "\"", "Y", "'", "E", "Yu", "Ya" };
-      string[] lat_low = { "a", "b", "v", "g", "d", "e", "yo", "zh", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "\"", "y", "'", "e", "yu", "ya" };
+      string[] lat_up = { "A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "Kh", "Ts", "Ch", "Sh", "Shch", "", "Y", "", "E", "Yu", "Ya" };
+      string[] lat_low = { "a", "b", "v", "g", "d", "e", "yo", "zh", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "", "y", "", "e", "yu", "ya" };
       string[] rus_up = { "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я" };
       string[] rus_low = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я" };
       for (int i = 0; i <= 32; i++)
