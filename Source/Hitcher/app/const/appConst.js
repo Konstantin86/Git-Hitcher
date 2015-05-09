53687ded9476c6ba10e1629ab346b3593c9cf362
Konstantin86 <kostyan22@gmail.com>
2015-05-10 01:50:14 +0300

    Bugfixing

--- Source/Hitcher/app/const/appConst.js
+++ Source/Hitcher/app/const/appConst.js
@@ -6,9 +6,9 @@
 
 app.constant("appConst", {
 
-    //serviceBase: "https://hitcher.azurewebsites.net/",
-    serviceBase: "http://localhost:49976/",
-    cdnBase: "http://az757169.vo.msecnd.net/",
-    cdnMediaBase: "http://az757169.vo.msecnd.net/media/",
+    serviceBase: "https://hitcher.azurewebsites.net/",
+    //serviceBase: "http://localhost:49976/",
+    cdnBase: "http://az757169.vo.msecnd.net/blob/",
+    cdnMediaBase: "http://az757169.vo.msecnd.net/blob/media/",
     userPhotoWidth: "310"
 });
