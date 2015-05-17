3a959a530b0af3f20d1b9f14c6a6120ed90e9b34
Konstantin86 <kostyan22@gmail.com>
2015-05-17 03:12:57 +0300

    Updated worker

--- Source/Hitcher.WebJob/Program.cs
+++ Source/Hitcher.WebJob/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Hitcher.Core.Services;
 using Hitcher.DataAccess;
 
 namespace Hitcher.WebJob
@@ -12,8 +13,18 @@ namespace Hitcher.WebJob
     static void Main(string[] args)
     {
       // Go over all existing route and remove outdated ones:
-      UnitOfWork _unitOfWork = new UnitOfWork(AppDbContext.Create());
-      _unitOfWork.RouteRepository.Delete(m => m.DueDate <= DateTime.Now);
+      RouteService routeService = new RouteService(new UnitOfWork(AppDbContext.Create()));
+
+      DateTime utcNow = DateTime.Now.ToUniversalTime();
+      var allRoutes = routeService.GetAll(false).Where(m => m.DueDate < utcNow).ToList();
+
+      foreach (var route in allRoutes)
+      {
+        routeService.Delete(route.Id);
+      }
+
+      //UnitOfWork _unitOfWork = new UnitOfWork(AppDbContext.Create());
+      //_unitOfWork.RouteRepository.Delete(m => m.DueDate <= DateTime.Now);
     }
   }
 }
