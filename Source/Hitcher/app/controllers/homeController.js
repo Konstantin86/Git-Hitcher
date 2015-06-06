22a39ebbfa5edb5f86a541b63eb175545821e272
Konstantin86 <kostyan22@gmail.com>
2015-06-06 12:21:37 +0300

    Added landing page

--- Source/Hitcher/app/controllers/homeController.js
+++ Source/Hitcher/app/controllers/homeController.js
@@ -6,6 +6,55 @@
 
 "use strict";
 
-app.controller("indexController", function ($scope, $location, $aside, authService, userService, routeService, mapService, statusService, chatService) {
+app.controller("homeController", function ($scope, $location, $aside, appConst, authService, userService, routeService, mapService, statusService, chatService) {
+    statusService.clear();
 
+    //$scope.welcomeText = msgConst.HOME_WELCOME;
+    //$scope.welcomeHeader = msgConst.HOME_WELCOME_HEADER;
+
+    var hostUri = appConst.serviceBase;
+
+    var slides = $scope.slides = [];
+
+    slides.push({
+        image: hostUri + "content/images/lp_2.jpg",
+        text: "Путешевствуй вместе с нами..."
+    });
+
+    slides.push({
+        image: hostUri + "content/images/lp_1.jpg",
+        text: "Альтернатива такси есть..."
+    });
+
+    //slides.push({
+    //    image: hostUri + "content/images/3-work-hard.jpg",
+    //    text: "Work hard..."
+    //});
+
+    //slides.push({
+    //    image: hostUri + "content/images/4-take-suggestions-from-community.jpg",
+    //    text: "Suggest with community..."
+    //});
+
+    //slides.push({
+    //    image: hostUri + "content/images/5-do-something-every-day.jpg",
+    //    text: "Do something every day..."
+    //});
+
+    //slides.push({
+    //    image: hostUri + "content/images/6-achieve-your-goals.jpg",
+    //    text: "Achieve your goals..."
+    //});
+
+    //slides.push({
+    //    image: hostUri + "content/images/7-share-your-success.jpg",
+    //    text: "Share your success..."
+    //});
+
+    //slides.push({
+    //    image: hostUri + "content/images/8-observe-places.jpg",
+    //    text: "Observe Places..."
+    //});
+
+    $scope.myInterval = 5000;
 });
---
+++ Source/Hitcher/app/directives/disableAnimation.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="~/scripts/angular.min.js"/>
+
+/// <reference path="~/app/app.js"/>
+
+"use strict";
+
+app.directive("disableAnimation", function ($animate) {
+    return {
+        restrict: "A",
+        link: function ($scope, $element, $attrs) {
+            $attrs.$observe("disableAnimation", function (value) {
+                $animate.enabled(!value, $element);
+            });
+        }
+    }
+});
