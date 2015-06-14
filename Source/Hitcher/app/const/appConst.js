6f140c04a41cc9b86e31799d99b6a91932025fa1
Konstantin86 <kostyan22@gmail.com>
2015-06-15 00:44:16 +0300

    Fixed references to blob storage cdn

--- Source/Hitcher/app/const/appConst.js
+++ Source/Hitcher/app/const/appConst.js
@@ -5,8 +5,10 @@
 "use strict";
 
 app.constant("appConst", {
-    cdnBase: "http://az757169.vo.msecnd.net/blob/",
-    cdnMediaBase: "http://az757169.vo.msecnd.net/blob/media/",
+    cdnBase: "http://az769387.vo.msecnd.net/",
+    //cdnBase: "http://az757169.vo.msecnd.net/blob/",
+    //cdnMediaBase: "http://az757169.vo.msecnd.net/blob/media/",
+    cdnMediaBase: "http://az769387.vo.msecnd.net/media/",
     userPhotoWidth: "310",
     chatPhotoWidth: "66"
 });
