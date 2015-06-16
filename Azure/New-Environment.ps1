b72004b05ee9669afa85ca21370595b510ec72ce
Konstantin86 <kostyan22@gmail.com>
2015-06-16 18:13:17 +0300

    Updating publish scripts

--- Azure/New-Environment.ps1
+++ Azure/New-Environment.ps1
@@ -39,9 +39,15 @@ New-AzureResourceGroup -Name $ResourceGroupName `
                        -Force `
 					             -Verbose;
 
+#Switch-AzureMode -Name AzureServiceManagement
+
 #Configure web app:
 #Enable web-sockets
-#configure custom domain
+#Set-AzureWebsite "SmartGo" -WebSocketsEnabled $true
+#http://blogs.msdn.com/b/cdndevs/archive/2015/04/23/azure-powershell-azure-websites-for-the-command-line-junkies-part-1.aspx
+#configure custom domains
+#Set-AzureWebsite -Name "ramisample" -HostNames @('www.abc.com', 'abc.com')
+#http://stackoverflow.com/questions/21036060/add-many-domains-to-an-azure-web-site
 
 #Configure sql
 #Set correct sql edition (standard)
@@ -49,12 +55,15 @@ New-AzureResourceGroup -Name $ResourceGroupName `
 
 #Configure storage:
 #Add container for storing app-specific blobs
+#Set-AzureSubscription -CurrentStorageAccountName "smartgostorage" -SubscriptionName (Get-AzureSubscription -current).SubscriptionName
+#New-AzureStorageContainer -Name "testcont"
+
+#Switch-AzureMode -Name AzureServiceManagement
 
 #cdn:
 #??? Investigate if its possible to add cdn via cmdlets
 
-#Set-AzureSubscription -CurrentStorageAccountName "smartgostorage" -SubscriptionName (Get-AzureSubscription -current).SubscriptionName
-#New-AzureStorageContainer -Name "testcont"
+
 
 #New-AzureStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountNameTest -Type Standard_GRS -Location $ResourceGroupLocation
 #Set-AzureStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -Type Standard_GRS
